7b512856807e76ad1d032a16c983aeb7e14b3a4c
NIU1746049 <hug.capafons@autonoma.cat>
2025-04-09 19:30:57 +0200

    Update tauler.h (revisar)

--- Damas/tauler.h
+++ Damas/tauler.h
@@ -8,9 +8,22 @@ using namespace std;
 class Tauler
 {
 public:
-	Tauler() : m_tornBlanques(true), m_filaFitxaSeleccionada(0), m_colFitxaSeleccionada(0) {}
+	Tauler() {
+		m_tornBlanques = true;
+		m_filaFitxaSeleccionada = 0;
+		m_colFitxaSeleccionada = 0;
+
+		/*
+		for (int i = 0; i < N_FILES;i++) {
+			for (int j = 0; j < N_COLUMNES;j++) {
+				Fitxa f;
+
+			}
+		}
+		*/
+	}
 	void inicialitza(const string& nomFitxer);
-	void actualitzaMovimentsValids() const; // Recorre todas las Fitxes y actualiza su variable Moviment con todos los movimientos validos
+	void actualitzaMovimentsValids();// Recorre todas las Fitxes y actualiza su variable Moviment con todos los movimientos validos
 	void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
 	bool mouFitxa(const Posicio& origen, const Posicio& desti);
 	string toString() const;
@@ -18,6 +31,21 @@ public:
 	
 	void seleccionaFitxa(const string& posFitxa);
 	void mouFitxa(int filaFitxa, int colFitxa);
+
+	void transportar(const Posicio& origen, const Posicio& desti);//fitxa1<->fitxa2 pos1<->pos2 (notis que al swapejar les fitxes ja hi ha hagut un canvi en les posicions per tant un segons canvi les deixa com estaven originalment.)
+
+	//Comprovar moviments
+	bool normalMoure(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+	bool normalMatar(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+	bool normalMatarMultiples(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+
+	bool damaMoure(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+	bool damaMatar(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+	bool damaMatarMultiples(Posicio posicioActual, Moviment movimentsValids[100], int& nMovimentsValids)const;
+
+
+
+
 
 private:
 	int m_filaFitxaSeleccionada;
