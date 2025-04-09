c192c17d440def3f638e6ba7a72ab00be429ff91
NIU1746049 <hug.capafons@autonoma.cat>
2025-04-09 20:00:01 +0200

    Update fitxes.h (revisar)ç

--- Damas/fitxes.h
+++ Damas/fitxes.h
@@ -13,28 +13,71 @@ typedef enum
 
 typedef enum
 {
+	COLOR_UNDEFINED,//Les fitxes empty no tindran color
 	COLOR_NEGRE,
-	COLOR_BLANC,
+	COLOR_BLANC
 } ColorFitxa;
+
 
 class Fitxa
 {
 public:
-	Fitxa() : m_viva(true) {}
+	/*Fitxa()
+	{
+		m_viva = true;
+		for (int i = 0;i < 100;i++) {
+			Moviment m
+			m_moviments[i] = m;
+		}
+		
+	
+	
+	}*/
+	Fitxa() {
+		m_colorFitxa = COLOR_UNDEFINED;
+		m_tipusFitxa = TIPUS_EMPTY;
+		m_viva = true;
+	}
+	Fitxa(TipusFitxa tipus, ColorFitxa color,Posicio pos) {
+		m_posicio = pos;
+		m_viva = true;
+		m_tipusFitxa = tipus;
+		if (tipus != COLOR_UNDEFINED) {
+			m_colorFitxa = color;
+
+		}
+		else {
+			m_colorFitxa = COLOR_UNDEFINED;//Programacio defensiva. A part fara mes rapid el programa perque NO haure de
+			//canviar el color per a cada fitxa que no sigui una fitxa (mentens?) dins de cada costat. Parteix taulell per la meitat i jasta.
+
+		}
+	}
 	//getters
-	TipusFitxa getTipusFitxa() { return m_tipusFitxa; }
-	ColorFitxa getColorFitxa() { return m_colorFitxa; }
+	TipusFitxa getTipusFitxa()const { return m_tipusFitxa; }
+	ColorFitxa getColorFitxa() const{ return m_colorFitxa; }
+	Posicio getPosicio()const { return m_posicio;}
+	int getNMoviments()const { return m_nMoviments;}
 
 
 	//setters
 	void setTipusFitxa(TipusFitxa tipusFitxa) { m_tipusFitxa = tipusFitxa; }
 	void setColorFitxa(ColorFitxa colorFitxa) { m_colorFitxa = colorFitxa; }
-
-	//void moure(int Fw, int L, int steps); // 1 = Fw/L && 0 = Bw/R   // <-- Creo que no hace falta, pq el movimiento se decidira a partir de Moviment. Que da la posicion absoulta, no relativa (se puede hacer con un set)
-
+	void setPosicio(Posicio posicio) { m_posicio = posicio; }
+	void setMovimentPos(int pos, Moviment m) { m_moviments[pos] = m; }
+	void setNMovimentsValids(int nMv) { m_nMoviments = nMv; }
+	
+	
+	
+	
+	//AQUEST PROBLEMATIC DE DALT
+	
+	
+	
 private:
-	Posicio m_posicio;//Borra comentario cuando lo veas
+	Posicio m_posicio;
 	Moviment m_moviments[100];
+	int m_nMoviments = 0;
+
 	TipusFitxa m_tipusFitxa;
 	ColorFitxa m_colorFitxa;
 	bool m_viva;
